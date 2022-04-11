d122738cfad572e202878281aa5926016dbf5e9e
aliklc44 <101723891+aliklc44@users.noreply.github.com>
2022-04-11 14:43:47 +0300

    ali-getTotal

--- src/Projects_01/getTotal.java
+++ src/Projects_01/getTotal.java
@@ -4,9 +4,7 @@ import java.util.Scanner;
 
 public class getTotal {
 
-
       /*
-
         Given three positive Strings
         remove all the non numeric numbers.
         change the Strings to int
@@ -20,9 +18,6 @@ public class getTotal {
 
         NOTE : if the output less then 0 change output to -1
 
-     */
-
-    /*
     3 tane pozitif String verildiğinde
     sayısal olmayan tüm karakterleri kaldırın.
     Stringleri int e çevirin
@@ -35,12 +30,35 @@ public class getTotal {
        output ----> 65; olmali
 
        NOT : Eğer output 0 dan küçük ise outputu -1 e cevir
-$15
 
    */
     public static void main(String[] args) {
 
 
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("1. degeri giriniz");
+        String num1 = scan.next();
+
+        System.out.println("2. degeri giriniz");
+        String num2 = scan.next();
+
+        System.out.println("3. degeri giriniz");
+        String num3 = scan.next();
+
+        int sayi1=Integer.parseInt(num1.replaceAll("[$A-Za-z]", ""));
+        int sayi2=Integer.parseInt(num2.replaceAll("[$A-Za-z]", ""));
+        int sayi3=Integer.parseInt(num3.replaceAll("[$A-Za-z]", ""));
+
+        int toplam = (sayi1 + sayi2 + sayi3);
+
+        if (toplam > 0) {
+            System.out.println(toplam);
+        } else {
+            System.out.println(-1);
+        }
+
     }
 }
+
 
