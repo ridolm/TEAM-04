cbf26cfc92a5deddad0651d01753815927badcfb
İlhan Akyüz <ilhanakyz3838@gmail.com>
2022-04-19 22:00:02 +0300

    minNumber

--- src/Projects_02/minNumber.java
+++ src/Projects_02/minNumber.java
@@ -21,12 +21,23 @@ public class minNumber {
 
      */
 
+        public static void main(String[] args) {
 
+            // Find the max. element in 2 dimensional array
+            int a[][] = { {5, 0}, {-5, 34}, {65, 22, 31} };
 
+            int x = Integer.MIN_VALUE;
 
-    public static void main(String[] args) {
+            for(int [] w: a){
+                for(int m : w){
+                    if(x<m){
+                        x=m;
+                    }
+                }
+            }
+            System.out.println(x);
 
-
+        }
 }
 
-}
+
