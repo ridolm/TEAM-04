f75e5c94246c9e9f85c1f93caab8eb559dcfcf89
DzKKaya <kayalik007@gmail.com>
2022-04-19 22:20:33 +0300

    ilk proje

--- src/Projects_01/totalLetterCountCheck.java
+++ src/Projects_01/totalLetterCountCheck.java
@@ -18,19 +18,13 @@ public class totalLetterCountCheck {
      */
 
     public static void main(String[] args) {
-<<<<<<< HEAD
-        Scanner scanner =new Scanner(System.in);
-        System.out.print("Bir kelime giriniz : ");
-        String kelime=scanner.nextLine();
 
-        if (kelime.length()%2==0){
-=======
 Scanner scanner =new Scanner(System.in);
         System.out.print("Bir kelime giriniz : ");
         String cumle=scanner.nextLine();
 
         if (cumle.length()%2==0){
->>>>>>> esat
+
             System.out.println(false);
         }else {
             System.out.println(true);
